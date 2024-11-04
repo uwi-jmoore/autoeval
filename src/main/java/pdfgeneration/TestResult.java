90c5895aec188c0f471f0b0ba6d508b77ee3ade1
juumen <157774312+juumen@users.noreply.github.com>
2024-11-04 19:15:17 -0400

    Update TestResult.java

--- src/main/java/pdfgeneration/TestResult.java
+++ src/main/java/pdfgeneration/TestResult.java
@@ -1,1 +1,20 @@
+package pdfgeneration;
+
+public class TestResult {
+    private final String testName;
+    private final boolean passed;
+
+    public TestResult(String testName, boolean passed) {
+        this.testName = testName;
+        this.passed = passed;
+    }
+
+    public String getTestName() {
+        return testName;
+    }
+
+    public boolean isPassed() {
+        return passed;
+    }
+}
 
