1b300ba1b97cd6fe22fe570a0cf5f6f8ebb25e04
juumen <157774312+juumen@users.noreply.github.com>
2024-11-04 19:14:43 -0400

    Update ReportGeneratorInterface.java

--- src/main/java/pdfgeneration/ReportGeneratorInterface.java
+++ src/main/java/pdfgeneration/ReportGeneratorInterface.java
@@ -1,1 +1,8 @@
+package pdfgeneration;
+
+import java.util.List;
+
+public interface ReportGeneratorInterface {
+    void generateReport(List<TestResult> testResults, int totalPoints, String outputFileName);
+}
 
