a4dbba9f13b80bca4b3df289f1381ed8a4391b02
Jarrod Moore <109738746+uwi-jmoore@users.noreply.github.com>
2024-11-05 21:35:45 -0400

    Code for Loading classes at runtime using ClassLoader and unit test added

---
+++ src/main/java/assignmentevaluator/classloader/AssignmentClassLoader.java
@@ -0,0 +1,18 @@
+package assignmentevaluator.classloader;
+
+import java.io.IOException;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class AssignmentClassLoader extends ClassLoader{
+
+    public Class<?> loadClassFromFileAlpha(String classFilePath) throws IOException, ClassNotFoundException {
+        Path path = Paths.get(classFilePath);
+        byte[] classBytes = Files.readAllBytes(path);
+
+        // Define the class
+        return defineClass(null, classBytes, 0, classBytes.length);
+    }
+}
