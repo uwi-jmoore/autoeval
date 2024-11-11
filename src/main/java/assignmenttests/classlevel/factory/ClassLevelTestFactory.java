9517ebd2bb0881c62483456d07d87ffb0b3f11cb
Jarrod Moore <109738746+uwi-jmoore@users.noreply.github.com>
2024-11-10 20:00:38 -0400

    more changes to AttributeSignatureTest

--- src/main/java/assignmenttests/classlevel/factory/ClassLevelTestFactory.java
+++ src/main/java/assignmenttests/classlevel/factory/ClassLevelTestFactory.java
@@ -3,7 +3,7 @@ package assignmenttests.classlevel.factory;
 import assignmenttests.classlevel.ClassTest;
 
 public abstract class ClassLevelTestFactory {
-    public static ClassTest createClassTest(){
+    public static ClassTest createClassTest(String... type){
         return null;
     }
 }
