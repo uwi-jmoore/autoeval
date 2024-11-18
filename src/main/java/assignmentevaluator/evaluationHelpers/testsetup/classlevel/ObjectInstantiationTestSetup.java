ba9f2ffc39e0924606470c52da31191e2b3900c0
moniquebritto21 <moniquencbritto@gmail.com>
2024-11-17 20:38:13 -0400

    Java Docs

--- src/main/java/assignmentevaluator/evaluationHelpers/testsetup/classlevel/ObjectInstantiationTestSetup.java
+++ src/main/java/assignmentevaluator/evaluationHelpers/testsetup/classlevel/ObjectInstantiationTestSetup.java
@@ -1,25 +1,53 @@
 package assignmentevaluator.evaluationHelpers.testsetup.classlevel;
 
-import assignmentevaluator.evaluationHelpers.testsetup.TestSetupService;
-import assignmenttests.classlevel.products.simulation.InstantiationMethodTarget;
-
 import java.io.File;
 import java.util.HashMap;
 
+import assignmentevaluator.evaluationHelpers.testsetup.TestSetupService;
+import assignmenttests.classlevel.products.simulation.InstantiationMethodTarget;
+
+/**
+ * Configures the setup for object instantiation tests.
+ * This class is responsible for setting up the object to be instantiated, 
+ * the method to instantiate the object, and the file associated with the test.
+ */
 public class ObjectInstantiationTestSetup extends TestSetupService {
+
+    /**
+     * Constructs a new `ObjectInstantiationTestSetup` instance.
+     * Initializes the mappings for the object, target method, and file as null.
+     */
     public ObjectInstantiationTestSetup(){
         map = new HashMap<>();
-        map.put("object",null);
-        map.put("target",null);
-        map.put("file",null);
+        map.put("object", null);
+        map.put("target", null);
+        map.put("file", null);
     }
+
+    /**
+     * Sets the object to be instantiated in the test.
+     * 
+     * @param inObj the object that is to be instantiated.
+     */
     public void setObject(Object inObj){
-        map.put("object",inObj);
+        map.put("object", inObj);
     }
+
+    /**
+     * Sets the target method used for object instantiation in the test.
+     *
+     * @param inMethod the method responsible for instantiating the object.
+     */
     public void setMethodTarget(InstantiationMethodTarget inMethod){
-        map.put("target",inMethod);
+        map.put("target", inMethod);
     }
+
+    /**
+     * Sets the file associated with the instantiation test.
+     *
+     * @param directory the file or directory related to the instantiation test.
+     */
     public void setFile(File directory){
-        map.put("file",directory);
+        map.put("file", directory);
     }
 }
